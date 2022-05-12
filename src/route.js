b018f66454f04f552aa702989b1bd792a64d3627
RSpenc123 <RSpenc12345@yahoo.com>
2022-05-12 14:26:23 -0700

    nav

--- src/route.js
+++ src/route.js
@@ -3,11 +3,13 @@ import {Switch, Route} from 'react-router-dom';
 
 import Homepage from "./Components/Homepage"
 import Class from "./Components/class"
+import Nav from "./Components/nav"
 
 export default (
     <Switch>
         <Route exact path = '/' component={Homepage}/>
         <Route path = "/Class1" component={Class}/>
+        <Route path = "/Nav" component={Nav}/>
         
     </Switch>
 )
